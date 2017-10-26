0f344216ee38e32c0a324e530bfca93ba898b6ae
stouduo <stouduo@163.com>
2017-10-26 23:54:36 +0800

    新增修改

--- src/main/resources/static/js/addresses.js
+++ src/main/resources/static/js/addresses.js
@@ -1,16 +1,13 @@
+window.userAddress = '';//在JS中存储用户完整地址
 ;(function () {
     $.getJSON('/js/address.json', function (address) {
-        var form = layui.form;
-        var $detail = $('#user-detail-address');
-        var province, city, block, details = '';
-        var $province = $("#province");
-        var $city = $("#city");
-        var $block = $("#block");
+        var province,city,block,details='';
+        var $province = $(".province");
         var getProvinceList = function () {
             var list = [];
-            for (var i = 0, l = address.length; i < l; i++) {
-                if ('0' == address[i].parentId) {
-                    list.push($.extend({}, address[i]));
+            for(var i=0,l=address.length;i<l;i++){
+                if('0' ==address[i].parentId){
+                    list.push($.extend({},address[i]));
                 }
             }
             return list;
@@ -20,16 +17,15 @@
          * @param nowDistrictAreaName 当前地区的名字
          */
         var getNextDistrictList = function (nowDistrictAreaName) {
-//                    {"areaCode":"1","areaName":"北京","areaPinYin":"bei jing","parentId":"0"}
-            var list = [], id = '';
-            for (var i = 0, l = address.length; i < l; i++) {
-                if (address[i].areaName === nowDistrictAreaName) {
-                    id = address[i].areaCode;
+            var list = [],id='';
+            for(var i=0,l=address.length;i<l;i++){
+                if(address[i].areaName === nowDistrictAreaName){
+                    id=address[i].areaCode;
                 }
             }
-            for (var i = 0, l = address.length; i < l; i++) {
-                if (id === address[i].parentId) {
-                    list.push($.extend({}, address[i]));
+            for(var i=0,l=address.length;i<l;i++){
+                if(id===address[i].parentId){
+                    list.push($.extend({},address[i]));
                 }
             }
             return list;
@@ -40,26 +36,27 @@
          * @param defaultText
          * @returns {string}
          */
-        var createOptions = function (addressList, defaultText) {
+        var createOptions = function (addressList,defaultText) {
             var template = '<option value="q_MyValue">q_MyText</option>';
-            var html = '<option value="-1" >' + (defaultText || "--请选择--") + '</option>';
-            for (var i = 0, l = addressList.length; i < l; i++) {
-                html += template.replace("q_MyValue", addressList[i].areaName).replace("q_MyText", addressList[i].areaName);
+            var html = '<option value="" >'+(defaultText || "--请选择--")+'</option>';
+            for(var i=0,l=addressList.length;i<l;i++){
+                html+= template.replace("q_MyValue",addressList[i].areaName).replace("q_MyText",addressList[i].areaName);
             }
             return html;
         }
-        var selectHandler = function (data, level) {
+        var selectHandler = function (data,level) {
             window.data = data;
             var areaName = data.value;
             var list = getNextDistrictList(areaName);
             var html = "";
-            var $select = null;
-            if (!level || level == 0) {
-                html = createOptions(list, "市");
-                $select = $city;
-            } else {
-                html = createOptions(list, "市区/县");
-                $select = $block;
+            var $select = $(data.elem).parent().next();
+            if(!level || level == 0){
+                html =createOptions(list, "市");
+                $select.next().find(".block").html("<option value=''>市区/县</option>");
+                $select = $select.find(".city");
+            }else{
+                html = createOptions(list,"市区/县");
+                $select = $select.find(".block");
             }
             $select.html(html);
             form.render();
@@ -67,25 +64,19 @@
 
         var init = function () {
             var list = getProvinceList();
-            var html = createOptions(list, "省/自治区/直辖市");
+            var html = createOptions(list,"省/自治区/直辖市");
             $province.html(html);
             form.render();
         };
-        $detail.blur(function () {
-            details = this.value;
-        });
-        form.on('select(province)', function (data) {
-//                    alert(data)
+        form.on('select(province)', function(data){
             province = data.value;
-            selectHandler(data, 0);
+            selectHandler(data,0);
         });
-        form.on('select(city)', function (data) {
-//                    alert(data)
+        form.on('select(city)', function(data){
             city = data.value;
-            selectHandler(data, 1);
+            selectHandler(data,1);
         });
-        form.on('select(block)', function (data) {
-//                    alert(data)
+        form.on('select(block)', function(data){
             block = data.value;
         });
         init();
