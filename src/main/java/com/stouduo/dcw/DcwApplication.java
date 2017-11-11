a0879958488f61781e094888f8b9a9bbb455630a
stouduo <stouduo@qq.com>
2017-11-12 00:07:43 +0800

    新增修改

--- src/main/java/com/stouduo/dcw/DcwApplication.java
+++ src/main/java/com/stouduo/dcw/DcwApplication.java
@@ -2,12 +2,18 @@ package com.stouduo.dcw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class DcwApplication {
+public class DcwApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DcwApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DcwApplication.class);
     }
 }
