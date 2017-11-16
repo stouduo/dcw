f55d75c00c6992e923347e5e08f3a75a26a1a636
stouduo <stouduo@qq.com>
2017-11-16 19:43:35 +0800

    新增修改

--- src/main/java/com/stouduo/dcw/DcwApplication.java
+++ src/main/java/com/stouduo/dcw/DcwApplication.java
@@ -2,14 +2,20 @@ package com.stouduo.dcw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class DcwApplication {
+public class DcwApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DcwApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DcwApplication.class);
     }
 }
